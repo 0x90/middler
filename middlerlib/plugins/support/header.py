ff8206002b33163a4e104e2946e43d9d930a9df7
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-04 23:09:58 +0000

    Make JLog import work...

--- middlerlib/plugins/support/header.py
+++ middlerlib/plugins/support/header.py
@@ -1,6 +1,6 @@
 # This file contains API helper routnes for checking and changing HTTP headers.
 
-from JLog import *
+from middlerlib.JLog import *
 
 headertest_stdmatch = (("Content-type","text/html"),)
 
