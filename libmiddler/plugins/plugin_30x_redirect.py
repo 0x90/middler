d83403d0d3f9d77e02d510e94e6dc483953ab53b
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-13 06:15:50 +0000

    Added a 301/307 redirect plugin that uses the status code to redirect the user.

---
+++ libmiddler/plugins/plugin_30x_redirect.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import urllib2
+import re
+import libmiddler.api.header as header
+#import libmiddler.api.register_http_plugin as register_http_plugin
+
+### FUNCTION TO MANIPULATE CLIENT REQUEST
+def doRequest(session, request_header, data):
+  changed = 0
+  stop = 0
+  return(request_header, data, changed, stop)
+
+### FUNCTION TO MANIPULATE SERVER RESPONSE
+def doResponse(session, request_header, response_header, data):
+  changed = 0
+  stop = 0
+  i = 1
+
+  # Check to see if we've got the right target site
+
+  target = "www.cnn.com"
+  request_match = (("Host",target),)
+
+  if header.headertest(request_header,request_match):
+
+      # We could make sure only to do this if the browser was getting a web page.
+      response_match = (("Content-type","TEXT/HTML"),)
+      if header.headertest( response_header,response_match ):
+
+          # Check the response code line.
+          response_code_line = response_header[0][1]
+          (protover,response_code,reason) = response_code_line.split(" ")
+
+          print ("Response code line had these elements --%s-- --%s-- --%s\n" % (protover,response_code,reason) )
+
+          # Make sure we are only doing this on a 200 message.
+          if response_code != "200":
+              return(response_header, data, changed, stop)
+
+          # Change the response code to a 30x redirect.
+
+          # Choose one of these two.
+          response_code = 307
+          reason = "Temporary Redirect"
+          #response_code = 301
+          #reason = "Moved Permanently"
+
+          # Check if there is a Location header already?
+          # TODO: make a routine that inserts a new header after a specific line.
+          if header.headerget("Location") and redirect_url:
+              header.headerfix( response_header, "Location", redirect_url )
+          else:
+              response_header.append( ("Location",redirect_url) )
+
+          # We have changed the header and we don't want any other plugins to touch it.
+          # TODO: Decide on how to do priority/dependencies/ordering so redirects go first.
+
+          changed = 1
+          stop = 1
+
+  return(response_header, data, changed, stop)
