b2e316d58ab755574c9e6e44fdf2d79454b37016
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-26 22:53:33 +0000

    Added a print to The Middler tells you that it's handling a connection

--- libmiddler/proxies/http/http_proxy.py
+++ libmiddler/proxies/http/http_proxy.py
@@ -617,6 +617,7 @@ class MiddlerHTTPProxy(SocketServer.StreamRequestHandler):
 
             port = 80
             ml.jjlog.debug("%s is requesting %s:%s" % (self.client_address[0], desthostname, port))
+            print("%s is requesting %s:%s" % (self.client_address[0], desthostname, port))
 
             try:
                 if method == "POST":
