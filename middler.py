e46fe4f9c02f1c725fe1b1932d0bc36e3dad942c
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-28 18:07:13 +0000

    Updated for SIP support

--- middler.py
+++ middler.py
@@ -23,7 +23,6 @@ def parseCommandLineFlags():
                                     help="HTTP should listen on this port",default="80")
     parser.add_option("-A", "--arpspoof_off", action="store_true", dest="toggle_arpspoof_off", default=False,
                                     help="turn off ARP spoofing, so The Middler doesn't broadcast ARP replies claiming the router's IP address")
-    
     (options,args)=parser.parse_args()
 
     return (options,args)
@@ -89,6 +88,11 @@ if __name__ == '__main__':
     # Initialize Logging - open files for writing and create thread locks.
     ml.jjlog.initialize()
 
+    # Announce ourselves
+    print "=================================="
+    print("The Middler - HTTP and SIP Edition")
+    print "==================================\n\n"
+
     # Start up the firewalling and routing to    send traffic to us.
     #from ml.Middler_Firewall import startRedirection,stopRedirection
     ml.traffic_capture.start()
@@ -112,19 +116,65 @@ if __name__ == '__main__':
         # Doing the latter requires working with shared memory and command channels.
         #ml.set_up_arpspoofing(target_host="ALL",interface="defaultroute",impersonated_host="defaultrouter")
 
-    # Start up the multi-threaded proxy
-    ml.jjlog.debug("Activating proxy\n")
+    #
+    # Set up a data structure to hold the threads that are running our servers
+    #
 
+    proxy_threads = []
+
+    #
+    # Start up the multi-threaded HTTP proxy
+    #
+
+    ml.jjlog.debug("Activating HTTP proxy\n")
+
+    # Import HTTP proxy modules
     import libmiddler.proxies
     import libmiddler.proxies.http
     import libmiddler.proxies.http.http_proxy
 
-    server = libmiddler.proxies.http.http_proxy.ThreadedTCPServer((ml.hostname,ml.port), libmiddler.proxies.http.http_proxy.MiddlerHTTPProxy)
-    print("Middler Started and Proxying")
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.setDaemon(True)
-    server_thread.start()
-    print "Server loop running in thread:", server_thread.getName()
+    # Stage an HTTP proxy on port 80
+    # Start up HTTP proxies on TCP port 80
+    for tcp_port in (ml.port,):
+        server = libmiddler.proxies.http.http_proxy.ThreadedTCPServer((ml.hostname,ml.port), libmiddler.proxies.http.http_proxy.Middler_HTTP_Proxy)
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.setDaemon(True)
+        proxy_threads.append(server_thread)
+        server_thread.start()
+        print "HTTP proxy loop running in thread %s listening on port %d" % (server_thread.getName(),ml.port)
+
+    #
+    # SIP Proxies
+    #
+
+    # Import SIP proxy modules
+    import libmiddler.proxies.sip
+    import libmiddler.proxies.sip.sip_proxy
+
+    # Now start up a SIP UDP proxy on 5060, 5061, 10000, and 64064
+    for udp_port in 5060, 5061, 10000, 64064:
+        sip_udp_proxy = libmiddler.proxies.sip.sip_proxy.ThreadedUDPServer((ml.hostname,udp_port), libmiddler.proxies.sip.sip_proxy.Middler_SIP_UDP_Proxy)
+        sip_udp_thread = threading.Thread(target=sip_udp_proxy.serve_forever)
+        sip_udp_thread.setDaemon(True)
+        proxy_threads.append(sip_udp_thread)
+        sip_udp_thread.start()
+        print "SIP UDP proxy running in thread %s listening on port %d" % (sip_udp_thread.getName(),udp_port)
+    #
+    # Future feature: TCP proxy too, implementing the TCP and UDP versions as subclasses of a wider class.
+    #
+
+    #
+    # Primary difference is having to read Content-Length.
+    #
+
+    # Now start up a SIP TCP proxy on 5060 and 5061
+    #SIP_tcp_proxy = libmiddler.proxies.sip.sip_proxy.ThreadedTCPServer((ml.hostname,5060), libmiddler.proxies.sip.sip_proxy.MiddlerSIPTCPProxy)
+    #sip_tcp_thread = threading.Thread(target=SIP_tcp_proxy.serve_forever)
+    #sip_tcp_thread.setDaemon(True)
+    #proxy_threads.append(sip_tcp_thread)
+    #sip_tcp_thread.start()
+    #print "SIP TCP proxy running in thread: ", sip_tcp_thread.getName()
+
 
     while True:
         pass
