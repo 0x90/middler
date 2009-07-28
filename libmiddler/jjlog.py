3a29a3eecd6b005e29d4107f8b13fcf25924606e
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-28 18:08:41 +0000

    Updated logging to speed up proxy if you don't want logs.

--- libmiddler/jjlog.py
+++ libmiddler/jjlog.py
@@ -2,6 +2,11 @@
 
 # This file is entirely dedicated to providing thread-safe logging capability to the program.
 
+# The thread locks, though, are almost certainly making the proxy slower.
+
+# Let's only log to the debug log file when DEBUG_LOGGING is 1 or higher.
+# Let's log to the screen when DEBUG_LOGGING is 2 or higher.
+
 # JLog!
 
 import thread
@@ -12,20 +17,29 @@ DEVELOPER_LOGGING=0
 #global DEBUG_LOGGING
 DEBUG_LOGGING=0
 
-dev_log_file =open("developerlog.txt","w")
-debug_log_file =open("debuglog.txt","w")
-dev_log_lock=thread.allocate_lock()
-debug_log_lock=thread.allocate_lock()
+if DEVELOPER_LOGGING >0:
+    dev_log_file =open("developerlog.txt","w")
+    dev_log_lock=thread.allocate_lock()
+
+if DEBUG_LOGGING >0:
+    debug_log_file =open("debuglog.txt","w")
+    debug_log_lock=thread.allocate_lock()
 
 def initialize():
-    dev_log_file = open("developerlog.txt","w")
-    debug_log_file = open("debuglog.txt","w")
-    dev_log_lock = thread.allocate_lock()
-    debug_log_lock = thread.allocate_lock()
+    if DEBUG_LOGGING >0:
+        dev_log_file = open("developerlog.txt","w")
+        dev_log_lock = thread.allocate_lock()
+
+    if DEVELOPER_LOGGING >0:
+        debug_log_file = open("debuglog.txt","w")
+        debug_log_lock = thread.allocate_lock()
 
 def stop():
-    dev_log_file.close()
-    debug_log_file.close()
+    if DEBUG_LOGGING >0:
+        debug_log_file.close()
+
+    if DEVELOPER_LOGGING >0:
+        dev_log_file.close()
 
 def jlog_debug_on():
     DEBUG_LOGGING = 1
@@ -42,40 +56,41 @@ def log(message,add_newline=1):
 def developer_log(message,add_newline=1):
     """This writes a log to a file and, if DEVELOPER_LOGGING is set, to stdout."""
 
-    # Acquire the thread lock associated with logging
-    dev_log_lock.acquire()
+    if DEVELOPER_LOGGING >0:
 
-    # Write to the developer log.
-    dev_log_file.write( "%s: %s " % (time.ctime(time.time()),message) )
-    if add_newline==1:
-        dev_log_file.write("\n")
+        # Acquire the thread lock associated with logging
+        dev_log_lock.acquire()
 
-    # Stop writing to the developer file.
-    dev_log_lock.release()
+        # Write to the developer log.
+        dev_log_file.write( "%s: %s " % (time.ctime(time.time()),message) )
+        if add_newline==1:
+            dev_log_file.write("\n")
 
-    if DEVELOPER_LOGGING:
+        # Stop writing to the developer file.
+        dev_log_lock.release()
+
+    if DEVELOPER_LOGGING > 1:
         # Log to STDOUT
         log(message,add_newline)
 
 def debug(message,add_newline=1):
-    """This writes a log to a file and, if DEBUG_LOGGING is set, to stdout."""
+    """This writes a log to a file if DEBUG_LOGGING is >0 and also to stdout if DEBUG_LOGGING > 1."""
 
-    # Acquire the thread lock associated with logging
-    debug_log_lock.acquire()
-    # Write to the developer log.
-    debug_log_file.write("%s: %s " % (time.ctime(time.time()) , message) )
-    if (add_newline==1):
-        debug_log_file.write("\n")
+    if DEBUG_LOGGING >0:
 
-    # Stop writing to the developer file.
-    debug_log_lock.release()
+        # Acquire the thread lock associated with logging
+        debug_log_lock.acquire()
+        # Write to the developer log.
+        debug_log_file.write("%s: %s " % (time.ctime(time.time()) , message) )
+        if (add_newline==1):
+            debug_log_file.write("\n")
 
-    if DEBUG_LOGGING:
+        # Stop writing to the developer file.
+        debug_log_lock.release()
+
+    if DEBUG_LOGGING >1:
         log(message,add_newline)
-
-def debug_log_no_newline(message):
-    debug(message,0)
 
 def error_log(message,add_newline=1):
     log(message,add_newline)
-    # TODO : Add a log file and thread lock for this
+    # TODO : Add a log file and thread lock for this?
