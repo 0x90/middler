6eaf9462363829a754835437714199a9603a826a
jay.beale <jay.beale@fd9c7e10-fa1b-11dd-b507-8b4ecd5210be>
2009-07-28 17:50:33 +0000

    Updated for SIP

--- libmiddler/__init__.py
+++ libmiddler/__init__.py
@@ -3,4 +3,12 @@ import traffic_capture
 import plugins
 import proxies
 
+# Items we store, as a kind of global or class variable
 interface = ""
+redirection_ports = ()
+
+# IP address we're listening on, defaults to 0.0.0.0
+# hostname
+
+# Port we run the HTTP proxy on
+# port
